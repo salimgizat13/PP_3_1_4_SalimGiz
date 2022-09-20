68a4ea95c0f7e3f99eaa1d412b38ffe5ab2c8b82
Salim <salimgizat@mail.ru>
2022-09-21 00:46:37 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import ru.kata.spring.boot_security.demo.models.Role;
-
-public interface RoleService {
-    Role getRoleByName(String name);
-}
